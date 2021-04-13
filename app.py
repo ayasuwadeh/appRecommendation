ce8a4b690a6dd268a51f03ac79a859170bbea5a9
ayasuwadeh <ayafrhn@gmail.com>
2021-04-13 03:36:33 +0300

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask, request, jsonify
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
-import json
+from werkzeug.serving import WSGIRequestHandler
+
 from flask import Flask, request, jsonify
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
@@ -28,17 +25,13 @@ def hello_world():
     rrr = "hi from here"
     print(type(user_input))
     ds = pd.read_csv('C:\\Users\\Msys\\Desktop\\grad.csv', encoding='cp1252',
-                     usecols=['name', 'description', 'review', 'id'])
+                     )
     ds['description'] = ds['description'] + ds['review']
-    # ds.head()
-    # tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
-    # tfidf_matrix = tf.fit_transform(ds['review'])
     tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
     tfidf_matrix = tf.fit_transform(ds['description'])
-    # rnew = tf.fit_transform(rrr)
-    print(tfidf_matrix)
+    # print(tfidf_matrix)
     cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
-    print(cosine_similarities)
+    # print(cosine_similarities)
     results = {}
 
     for idx, row in ds.iterrows():
@@ -54,20 +47,38 @@ def hello_world():
     r = []
 
     def recommend(item_id, num):
-        print("Recommending " + str(num) + " products similar to " + item(item_id) + "...")
+        print("Recommending " + str(num) + " products similar to " + str(item_id) + "...")
         print("-------")
         recs = results[item_id][:num]
+        # print(recs)
         for rec in recs:
-            print("Recommended: " + item(rec[1]) + " (score:" + str(rec[0]) + ")")
+            print("Recommended: " + str(rec[1]) + " (score:" + str(rec[0]) + ")")
             r.append(rec[1].item())
 
     recommend(item_id=user_input, num=10)
+    resultDF = pd.DataFrame(columns=('id', 'name', 'lat', 'lng', 'formatted_address', 'phone',
+                                     'url', 'rate', 'description', 'image', 'category'))
+    for place in r:
+        # index = place['id']
+        resultDF = resultDF.append(
+            {'id': ds.iloc[place]['id'],
+             'name': ds.iloc[place]['name'],
+             'lng': ds.iloc[place]['lng'],
+             'lat': ds.iloc[place]['latitude'],
 
-    print(r)
-    print(type(r[0]))
+             'formatted_address': ds.iloc[place]['formatted address'],
+             'phone': ds.iloc[place]['phoneNumber'],
+             'url': ds.iloc[place]['url'],
+             'rate': ds.iloc[place]['rate'],
+             'description': ds.iloc[place]['description'],
+             'image': ds.iloc[place]['image'],
+             'category': ds.iloc[place]['category'],
 
-    d = {'QUERY': str(request.args['QUERY'])}
-    return jsonify(r)
+             }
+            , ignore_index=True)
+
+    json_result = json.dumps(resultDF.to_dict('records'))
+    return json_result
 
 # @app.route('/restaurants')
 # def hi():
@@ -75,7 +86,7 @@ def hello_world():
 
 
 @app.route('/restaurants', methods=['GET'])
-def hi():
+def restaurants():
     user_input = request.args['keywords']
     print(user_input)
 
@@ -144,7 +155,7 @@ def hi():
             resultDF = resultDF.append({'id': df.iloc[i[0]]['id'], 'name': df.iloc[i[0]]['name'], 'description': df.iloc[i[0]]['cuisine'],'rating': df.iloc[i[0]]['rating'], 'score': i[1]}, ignore_index=True)
             counter += 1
 
-            if counter > 20:
+            if counter > 10:
                 break
 
         json_result = json.dumps(resultDF.to_dict('records'))
@@ -154,6 +165,93 @@ def hi():
 
     return json_object
 
+
+@app.route('/places', methods=['GET'])
+def places():
+    # df = pd.read_csv('C:\\Users\Msys\\Desktop\\grad.csv',encoding='cp437', )
+    # df['description'] = df['description'] + df['review']
+    #
+    # df.dropna(subset=['description'])
+    # df = df[df['description'].notna()]
+    #
+    # def clear(city):
+    #     city = city.lower()
+    #     city = city.split()
+    #     city_keywords = [word for word in city if word not in stopwords.words('english')]
+    #     merged_city = " ".join(city_keywords)
+    #     return merged_city
+    #
+    # for index, row in df.iterrows():
+    #     clear_desc = clear(row['description'])
+    #     df.at[index, 'description'] = clear_desc
+    #
+    # updated_dataset = df.to_csv('places_data_cleared.csv')
+
+    def cosine_similarity_of(text1, text2):
+        first = re.compile(r"[\w']+").findall(text1)
+        second = re.compile(r"[\w']+").findall(text2)
+        vector1 = Counter(first)
+        vector2 = Counter(second)
+        common = set(vector1.keys()).intersection(set(vector2.keys()))
+        dot_product = 0.0
+        for i in common:
+            dot_product += vector1[i] * vector2[i]
+        squared_sum_vector1 = 0.0
+        squared_sum_vector2 = 0.0
+        for i in vector1.keys():
+            squared_sum_vector1 += vector1[i] ** 2
+        for i in vector2.keys():
+            squared_sum_vector2 += vector2[i] ** 2
+        magnitude = math.sqrt(squared_sum_vector1) * math.sqrt(squared_sum_vector2)
+        if not magnitude:
+            return 0.0
+        else:
+            return float(dot_product) / magnitude
+
+    def get_recommendations(keywords):
+
+        df = pd.read_csv('places_data_cleared.csv')
+
+        score_dict = {}
+
+        for index, row in df.iterrows():
+            score_dict[index] = cosine_similarity_of(row['description'], keywords)
+
+        sorted_scores = sorted(score_dict.items(), key=operator.itemgetter(1), reverse=True)
+
+        counter = 0
+
+        resultDF = pd.DataFrame(columns=('id', 'name', 'lat','lng','formatted_address','phone',
+                                         'url','rate','description', 'image', 'category','score'))
+        print(df['latitude'])
+        for i in sorted_scores:
+            resultDF = resultDF.append(
+                {'id': df.iloc[i[0]]['id'],
+                 'name': df.iloc[i[0]]['name'],
+                 'lng': df.iloc[i[0]]['lng'],
+                 'lat': df.iloc[i[0]]['latitude'],
+
+                 'formatted_address': df.iloc[i[0]]['formatted address'],
+                 'phone': df.iloc[i[0]]['phoneNumber'],
+                 'url': df.iloc[i[0]]['url'],
+                 'rate': df.iloc[i[0]]['rate'],
+                 'description': df.iloc[i[0]]['description'],
+                 'image': df.iloc[i[0]]['image'],
+                 'category': df.iloc[i[0]]['category'],
+
+                 'score': i[1]}, ignore_index=True)
+            counter += 1
+
+            if counter > 5:
+                break
+
+        json_result = json.dumps(resultDF.to_dict('records'))
+
+        return json_result
+
+    json_object = get_recommendations('museum')
+
+    return json_object
 
 @app.route('/cities', methods=['GET'])
 def city():
